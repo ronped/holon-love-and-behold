d8d776c6a1ed9f1c9380409b4fb4ede6bd33b913
Ronny Pedersen <ronny@pedersen.org>
2024-11-18 22:03:41 +0100

    [star geometry] Some WIP to support spiral shape

--- star_geometry.js
+++ star_geometry.js
@@ -3,7 +3,10 @@ import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
 
 class StarGeometry extends THREE.BufferGeometry {
 
-    constructor( points, color = null) {
+    static GEOMETRY_TYPE_DEFAULT = 0;
+    static GEOMETRY_TYPE_SPIRAL = 1;
+    
+    constructor( points, type = StarGeometry.GEOMETRY_TYPE_DEFAULT, color = null) {
 	super();
 
 	this.type = 'StarGeometry';
@@ -12,8 +15,6 @@ class StarGeometry extends THREE.BufferGeometry {
 	    this.color = new THREE.Color(color);
 	}
 
-	    
-	
 	var perlin = new ImprovedNoise( );
 
 	const positionBuf = new THREE.BufferAttribute( new Float32Array(3*points), 3),
@@ -22,21 +23,31 @@ class StarGeometry extends THREE.BufferGeometry {
 	      v = new THREE.Vector3( ),
 	      c = new THREE.Color( );
 
+	const arms = 3;
+	const points_per_arm = points/arms;
 	for( var i=0; i<points; i++ )
 	{
-	    v.randomDirection( ).setLength( 5*Math.pow(Math.random(),1/5) );
-	    v.x = v.x*(1+0.4*Math.sin(3*v.y)+0.4*Math.sin(2*v.z));
-	    v.y = v.y*(1+0.4*Math.sin(3*v.z)+0.4*Math.sin(2*v.x));
-	    v.z = v.z*(1+0.4*Math.sin(3*v.x)+0.4*Math.sin(2*v.y));
+	    if (type == StarGeometry.GEOMETRY_TYPE_DEFAULT){
+		v.randomDirection( ).setLength( 5*Math.pow(Math.random(),1/5) );
+		v.x = v.x*(1+0.4*Math.sin(3*v.y)+0.4*Math.sin(2*v.z));
+		v.y = v.y*(1+0.4*Math.sin(3*v.z)+0.4*Math.sin(2*v.x));
+		v.z = v.z*(1+0.4*Math.sin(3*v.x)+0.4*Math.sin(2*v.y));
+		var noise = (perlin.noise( v.x, v.y, v.z ) +
+			     perlin.noise( v.y, v.z, v.x ) +
+			     perlin.noise( v.z, v.x, v.y ));
+		
+		if( noise<0.3 ) {i--; continue;}
+	    } else {
+		const arm_pos = (i % points_per_arm)/points_per_arm;
+		const arm = Math.floor(i/points_per_arm);
+		v.x = (3**(2*arm_pos)-1)*Math.cos(4*Math.PI*arm_pos+2*arm*Math.PI/arms)+2*(Math.random()**3-0.5)*arm_pos**2;
+		v.y = (3**(2*arm_pos)-1)*Math.sin(4*Math.PI*arm_pos+2*arm*Math.PI/arms)+2*(Math.random()**3-0.5)*arm_pos**2;
+		v.z = 2*(Math.random()-0.5)*arm_pos;
+	    }
+
 	    positionBuf.setXYZ( i, v.x, v.y, v.z );
 	    normalBuf.setXYZ( i, v.x, v.y, v.z );
-	
-	
-	    var noise = (perlin.noise( v.x, v.y, v.z ) +
-			 perlin.noise( v.y, v.z, v.x ) +
-			 perlin.noise( v.z, v.x, v.y ));
 	    
-	    if( noise<0.3 ) {i--; continue;}
 	    if (this.color){
 		c.copy(this.color)
 	    } else {

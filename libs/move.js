7f2e15d10326a78e2516d22361efdf15e7dda51b
Ronny Pedersen <ronny@pedersen.org>
2025-01-01 16:05:38 +0100

    [move] Add support for specifying absolute time as start time
      - Also added a class CurveFunction that can be used to
        use functions for specifying movement paths.
      - Added support for named logging

--- libs/move.js
+++ libs/move.js
@@ -1,5 +1,25 @@
 import * as THREE from 'three';
 
+
+class CurveFunction {
+
+    constructor(func){
+	this.func = func;
+    }
+
+    getTangentAt(point){
+	const p1 = this.getPointAt(point);
+	const p0 = this.getPointAt(point-0.001);
+	return p1.sub(p0);
+    }
+
+    getPointAt(point){
+	return this.func(point);
+    }
+
+}
+
+
 class MOVE {
 
     static allMoves = [];
@@ -9,26 +29,76 @@ class MOVE {
 	})
     }	
     
-    constructor(obj, log=false){
+    constructor(obj, absoluteStartTime=false, log=false){
 	this.obj = obj;
 	this.clock = new THREE.Clock(false);
 	this.moveQueue = [];
 	this.curMove = 0;
+	this.absoluteStartTime = absoluteStartTime;
 	this.log = log;
+	if (this.log && typeof this.log !== 'string' && !(this.log instanceof String))
+	    this.log = obj.constructor.name;
 	MOVE.allMoves.push(this);
     }
 
-    run(func, startAfter=0){
+    // Get absolute end time of move back in the queue
+    getMoveEndTime(movesBack=0){
+	var endTime = 0;
+	if (this.moveQueue.length > movesBack){
+	    const prevMove = this.moveQueue[this.moveQueue.length-1-movesBack];
+	    endTime = prevMove.endTime;
+	}
+	return endTime;
+    }
+
+    // Get absolute start time of move back in the queue
+    getMoveStartTime(movesBack=0){
+	var startTime = 0;
+	if (this.moveQueue.length > movesBack){
+	    const prevMove = this.moveQueue[this.moveQueue.length-1-movesBack];
+	    startTime = prevMove.startTime;
+	}
+	return startTime;
+    }
+
+    run(func, startTime=0){
+	var startAfter;
+	var endTime;
+	if (this.absoluteStartTime){
+	    startAfter = startTime-this.getMoveEndTime();
+	    endTime = startTime;
+	    if (startAfter < 0)
+		console.error("run scheduled in past by " + startAfter + " seconds");
+	} else {
+	    startAfter = startTime;
+	    startTime = this.getMoveEndTime() + startAfter;
+	    endTime = startTime;
+	}
 	this.moveQueue.push({
 	    startAfter: startAfter,
+	    startTime: startTime,
+	    endTime: endTime,
 	    run: func
 	}) 
 	return this;
     }
 
-    loop(nrMoves, loopCnt=0, startAfter=0){
+    loop(nrMoves, loopCnt=0, startTime=0){
+	var startAfter;
+	if (this.absoluteStartTime){
+	    startAfter = startTime-this.getMoveEndTime();
+	    if (startAfter < 0)
+		console.error("loop scheduled in past by " + startAfter + " seconds");
+	} else {
+	    startAfter = startTime;
+	    startTime = this.getMoveEndTime() + startAfter;
+	}
+	const loopTime = (startTime - this.getMoveEndTime(nrMoves))*loopCnt;
+	const endTime = startTime + loopTime;
 	this.moveQueue.push({
 	    startAfter: startAfter,
+	    startTime: startTime,
+	    endTime: endTime,
 	    loop: nrMoves,
 	    loopCnt: loopCnt,
 	    curLoopCnt: 0
@@ -40,13 +110,13 @@ class MOVE {
     start(){
 	this.clock.start();
 	if (this.log)
-	    console.log("Time " + this.clock.elapsedTime + " Move started");
+	    console.log(this.log + ": Time " + this.clock.elapsedTime + " Move started");
 	return this;
     }
     
     stop(){
 	if (this.log)
-	    console.log("Time " + this.clock.elapsedTime + " Move stopped");
+	    console.log(this.log + ": Time " + this.clock.elapsedTime + " Move stopped");
 	this.clock.stop();
 	return this;
     }
@@ -83,10 +153,10 @@ class MOVE {
 		    queueElmt = this.moveQueue[this.moveQueue.length-lookback_idx];
 		else
 		    queueElmt = null;
-	    } while (queueElmt && ((queueElmt.pathCurve && queueElmt.pathCurve == MOVE.NO_CHANGE) || queueElmt.loop));
+	    } while (queueElmt && ((queueElmt.pathCurve && queueElmt.pathCurve == MOVE.NO_CHANGE) || queueElmt.loop || queueElmt.run ));
 
 	    if (queueElmt){
-		if (queueElmt.pathCurve instanceof THREE.Curve)
+		if (queueElmt.pathCurve instanceof THREE.Curve || queueElmt.pathCurve instanceof CurveFunction )
 		    return queueElmt.pathCurve.getPointAt(1.0);
 		else if (!queueElmt.pathCurve)
 		    return null;
@@ -107,10 +177,10 @@ class MOVE {
 		    queueElmt = this.moveQueue[this.moveQueue.length-lookback_idx];
 		else
 		    queueElmt = null;
-	    } while (queueElmt && ((queueElmt.rotationCurve && queueElmt.rotationCurve == MOVE.NO_CHANGE) || queueElmt.loop));
+	    } while (queueElmt && ((queueElmt.rotationCurve && queueElmt.rotationCurve == MOVE.NO_CHANGE) || queueElmt.loop || queueElmt.run));
 
 	    if (queueElmt){
-		if (queueElmt.rotationCurve.isObject3D)
+		if (queueElmt.rotationCurve && queueElmt.rotationCurve.isObject3D || queueElmt.rotationCurve instanceof THREE.Vector3)
 		    return queueElmt.rotationCurve;
 		else if (queueElmt.rotationCurve == MOVE.ROTATE_TO_TANGENT)
 		    return this.getRotationAngleCurve(queueElmt.pathCurve, 1.0);
@@ -125,7 +195,7 @@ class MOVE {
 	return new THREE.Vector3().setFromEuler(this.obj.rotation);
     }
 
-    to(pos, rotation=null, time=0, startAfter=0,  easing=null){
+    to(pos, rotation=null, time=0, startTime=0,  easing=null){
 	var pathCurve, rotationCurve;
 	const fromPos = this.getCurPosFromQueue();
 	const fromRotation = this.getCurRotationFromQueue(); 
@@ -160,9 +230,21 @@ class MOVE {
 	    rotationCurve = null;
 	}
 
+	var startAfter;
+	if (this.absoluteStartTime){
+	    startAfter = startTime-this.getMoveEndTime();
+	    if (startAfter < 0)
+		console.error("MOVE.to() scheduled in past by " + startAfter + " seconds");
+	} else {
+	    startAfter = startTime;
+	    startTime = this.getMoveEndTime() + startAfter;
+	}
+	const endTime = startTime + time;
 	this.moveQueue.push({
 	    startAfter: startAfter,
 	    toTime: time,
+	    startTime: startTime,
+	    endTime: endTime,
 	    easing: easing,
 	    pathCurve: pathCurve,
 	    rotationCurve: rotationCurve,
@@ -186,14 +268,12 @@ class MOVE {
 	var time = this.clock.getElapsedTime();
 
 	// Check if we are still waiting for the move to start
-	if (nextMove.startAfter && !nextMove.started){
-	    if (time >= nextMove.startAfter){
+	if (!nextMove.started){
+	    if (time >= nextMove.startTime){
+		nextMove.started = true;
 		// We should start move now
 		if (this.log)
-		    console.log("Time " + time + " Move " + this.curMove + " started");
-		nextMove.started = true;
-		time = 0;
-		this.clock.start();
+		    console.log(this.log + ": Time " + time + " Move " + this.curMove + " started");
 	    } else {
 		return;
 	    }
@@ -201,14 +281,20 @@ class MOVE {
 
 	// If this is a run then do the run and then stop
 	if (nextMove.run){
-	    this.clock.stop();
 	    nextMove.run(this.obj);
 	} else if (nextMove.loop){
 	    if (nextMove.loopCnt == 0 || nextMove.curLoopCnt < nextMove.loopCnt){
 		// Loop by rewinding the given number of moves
 		nextMove.curLoopCnt++;
 		this.curMove -= nextMove.loop;
-		this.clock.start();
+		const loopTime = nextMove.startTime - this.moveQueue[this.curMove].startTime;
+		for (let i=0; i<nextMove.loop+1; i++){
+		    const move = this.moveQueue[this.curMove+i];
+		    move.startTime += loopTime;
+		    move.endTime += loopTime;
+		}
+		if (this.log)
+		    console.log(this.log + ": Time " + this.clock.getElapsedTime() + " New Loop Iteration - Move " + this.curMove + " started");
 		return
 	    } else {
 		// Done with the loop so contineu to next move
@@ -216,7 +302,7 @@ class MOVE {
 	    }
 	} else {
 	    // Check move progress and apply easing
-	    var progress = Math.min(time,nextMove.toTime)/nextMove.toTime;
+	    var progress = Math.min(time-nextMove.startTime,nextMove.toTime)/nextMove.toTime;
 	    if (nextMove.easing){
 		progress = nextMove.easing(progress);
 	    }
@@ -236,16 +322,16 @@ class MOVE {
 		} else {
 		    var rotationCurve = nextMove.rotationCurve;
 		    if (nextMove.prevLookAtObject && nextMove.prevLookAtObject.isObject3D){
-			const fromRotation = new THREE.Vector3(this.rotation.x, this.rotation.y, this.rotation.z);
-			if (nextMove.rotationCurve.constructor === Array){
-			    rotation.unshift(fromRotation);
-			    rotationCurve = new THREE.CatmullRomCurve3(rotation);
-			} else if (nextMove.rotationCurve instanceof THREE.Vector3) {
-			    rotationCurve = new THREE.LineCurve3(fromRotation, rotation);
-			} else if (nextMove.rotationCurve == MOVE.NO_CHANGE){
+			const fromRotation = new THREE.Vector3(this.obj.rotation.x, this.obj.rotation.y, this.obj.rotation.z);
+			if (rotationCurve.constructor === Array){
+			    rotationCurve.unshift(fromRotation);
+			    rotationCurve = new THREE.CatmullRomCurve3(rotationCurve);
+			} else if (rotationCurve instanceof THREE.Vector3) {
+			    rotationCurve = new THREE.LineCurve3(fromRotation, rotationCurve);
+			} else if (rotationCurve == MOVE.NO_CHANGE){
 			    rotationCurve = new THREE.LineCurve3(fromRotation, fromRotation);
 			}
-			newtMove.rotationCurve = rotationCurve;
+			nextMove.rotationCurve = rotationCurve;
 			nextMove.prevLookAtObject = null;
 		    }
 		    if (rotationCurve)
@@ -254,7 +340,7 @@ class MOVE {
 	    }
 	    
 	    // Check if move is done
-	    if (time<nextMove.toTime)
+	    if ((time-nextMove.startTime)<nextMove.toTime)
 		// Not done yet
 		return;
 	}
@@ -263,13 +349,12 @@ class MOVE {
 	nextMove.started=false;
 	if (this.moveQueue.length > this.curMove+1){
 	    this.curMove++;
-	    this.clock.start();
 	    if (this.log)
-		console.log("Time " + this.clock.elapsedTime + " Waiting for move " + this.curMove);
+		console.log(this.log + ": Time " + this.clock.getElapsedTime() + " Waiting for move " + this.curMove);
 	} else {
 	    this.clock.stop();
 	}	
     }	
 }
 
-export {MOVE};
+export {MOVE, CurveFunction};
